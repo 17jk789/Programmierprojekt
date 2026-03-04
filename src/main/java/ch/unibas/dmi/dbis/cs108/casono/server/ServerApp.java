bf0dadc97739bf202a22b208e5d7c23858cb228c
Lars Simon Winzer <lars.winzer@stud.unibas.ch>
2026-03-04 11:30:37 +0100

    Fix: Typo in ServerApp class

--- src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/ServerApp.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/ServerApp.java
@@ -3,6 +3,6 @@ package ch.unibas.dmi.dbis.cs108.casono.server;
 public class ServerApp {
     public static void start(String arg) {
         int port = Integer.parseInt(arg);
-        System.out.println("You've selected the server. It will accept connections at port" + port);
+        System.out.println("You've selected the server. It will accept connections at port " + port);
     }
 }
