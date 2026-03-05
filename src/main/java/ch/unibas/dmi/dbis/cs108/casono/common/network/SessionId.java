e6b5cac53caceb870c26b8db268a1699333f0a85
Lars Simon Winzer <lars.winzer@stud.unibas.ch>
2026-03-05 13:28:04 +0100

    Add: SessionId class to uniquely identify session in SessionRegistry

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/common/network/SessionId.java
@@ -0,0 +1,22 @@
+package ch.unibas.dmi.dbis.cs108.casono.common.network;
+
+import java.util.UUID;
+
+public class SessionId {
+    /**
+     * The session id is used to identify a unique client connection in the SessionRegistry
+     */
+    private final UUID value;
+
+    public SessionId() {
+        this.value = UUID.randomUUID();
+    }
+
+    public SessionId(UUID value) {
+        this.value = value;
+    }
+
+    public UUID value() {
+        return value;
+    }
+}
