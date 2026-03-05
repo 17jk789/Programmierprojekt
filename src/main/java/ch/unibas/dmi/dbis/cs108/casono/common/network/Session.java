66925bc66541c5db0275380ccd42da8efb5e864f
Lars Simon Winzer <lars.winzer@stud.unibas.ch>
2026-03-05 13:46:10 +0100

    Add: Session interface all sessions need to conform to

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/common/network/Session.java
@@ -0,0 +1,13 @@
+package ch.unibas.dmi.dbis.cs108.casono.common.network;
+
+public interface Session {
+    /**
+     * Outlines an interface all session implementations have to conform to
+     */
+
+    SessionId id();
+
+    void send(byte[] payload);
+
+    void close();
+}
