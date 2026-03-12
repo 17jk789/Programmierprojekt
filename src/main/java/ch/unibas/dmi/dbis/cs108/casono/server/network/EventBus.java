e1029932d8940b7acd15821841ff1c6f2ff1aa31
Lars Simon Winzer <lars.winzer@stud.unibas.ch>
2026-03-12 19:27:29 +0100

    Fix: 'Potential null pointer access'

--- src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/network/EventBus.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/network/EventBus.java
@@ -2,6 +2,7 @@ package ch.unibas.dmi.dbis.cs108.casono.server.network;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.Consumer;
@@ -16,6 +17,7 @@ public class EventBus {
     }
 
     public <T extends Event> void publish(T event) {
+        Objects.requireNonNull(event, "event must not be null");
         List<Consumer<Object>> subscribers = handlers.get(event.getClass());
         if (subscribers != null) {
             subscribers.forEach(h -> h.accept(event));
