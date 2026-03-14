fa54fd75c16c22599302bb1de1f1f4e1030cf329
Lars Simon Winzer <lars.winzer@stud.unibas.ch>
2026-03-14 15:32:40 +0100

    Add: Tokenizer state and token representation

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/common/tokenizer/TokenType.java
@@ -0,0 +1,11 @@
+package ch.unibas.dmi.dbis.cs108.casono.common.tokenizer;
+
+/** Enumeration of token types used in the tokenizer. */
+public enum TokenType {
+    COMMAND,
+    KEY,
+    VALUE,
+    SEPARATOR,
+    NEWLINE,
+    EOF
+}
