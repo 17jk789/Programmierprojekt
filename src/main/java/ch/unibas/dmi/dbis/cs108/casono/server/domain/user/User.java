a7cdf995c4dbb533d1a80a71d0a76c641a7c2a65
Lars Simon Winzer <lars.winzer@stud.unibas.ch>
2026-03-15 18:44:03 +0100

    Add: Timestamp to User to store time of disconnect

--- src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/domain/user/User.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/domain/user/User.java
@@ -1,17 +1,20 @@
 package ch.unibas.dmi.dbis.cs108.casono.server.domain.user;
 
 import ch.unibas.dmi.dbis.cs108.casono.server.network.sessions.SessionId;
+import java.time.Instant;
 import java.util.Optional;
 
 public class User {
     private final UserId id;
     private final String name;
     private SessionId sessionId;
+    private Instant disconnectedAt;
 
     public User(UserId id, String name, SessionId sessionId) {
         this.id = id;
         this.name = name;
         this.sessionId = sessionId;
+        this.disconnectedAt = null;
     }
 
     public UserId getId() {
@@ -24,5 +27,19 @@ public class User {
 
     public Optional<SessionId> getSessionId() {
         return Optional.ofNullable(sessionId);
+    }
+
+    public Optional<Instant> getDisconnectedAt() {
+        return Optional.ofNullable(disconnectedAt);
+    }
+
+    public void reassignSession(SessionId sessionId) {
+        this.sessionId = sessionId;
+        this.disconnectedAt = null;
+    }
+
+    public void markDisconnected() {
+        this.sessionId = null;
+        this.disconnectedAt = Instant.now();
     }
 }
