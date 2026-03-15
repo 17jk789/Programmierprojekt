08dc94b4b09fc5552f5b9b6f47720aaa7dc636bf
Lars Simon Winzer <lars.winzer@stud.unibas.ch>
2026-03-15 13:38:59 +0100

    Style: Comply with Spotless
    Created issue #2 regarding this issue

--- src/test/java/ch/unibas/dmi/dbis/cs108/casono/server/tokenizer/TokenizerExceptionTest.java
+++ src/test/java/ch/unibas/dmi/dbis/cs108/casono/server/tokenizer/TokenizerExceptionTest.java
@@ -1,8 +1,7 @@
 package ch.unibas.dmi.dbis.cs108.casono.server.tokenizer;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TokenizerExceptionTest {
     @Test
