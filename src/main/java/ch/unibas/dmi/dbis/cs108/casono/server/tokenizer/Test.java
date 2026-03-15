4c0e6aeb3614223abd05485836dd8c41afdbd1c1
Lars Simon Winzer <lars.winzer@stud.unibas.ch>
2026-03-15 12:20:37 +0100

    Delete: Falsly added file for rudimentary quick testing

--- src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/tokenizer/Test.java
+++
@@ -1,7 +0,0 @@
-package ch.unibas.dmi.dbis.cs108.casono.server.tokenizer;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println(Tokenizer.tokenize("ARG=12"));
-    }
-}
