8fae4789ad3cb8f6e5b4a777e41e0e4bf3fda40b
Lars Simon Winzer <lars.winzer@stud.unibas.ch>
2026-03-15 19:37:43 +0100

    Add: Make UserRegistry use multiple Maps to accelerate lookups

--- src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/domain/user/UserRegistry.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/domain/user/UserRegistry.java
@@ -1,54 +1,25 @@
 package ch.unibas.dmi.dbis.cs108.casono.server.domain.user;
 
 import ch.unibas.dmi.dbis.cs108.casono.server.network.sessions.SessionId;
-
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class UserRegistry {
     private final ConcurrentHashMap<UserId, User> byId = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, User> byName = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<SessionId, User> bySessionId = new ConcurrentHashMap<>();
 
     public synchronized Optional<User> registerIfAvailable(String name, SessionId sessionId) {
-        Iterator<User> users = byId.elements().asIterator();
-        while(users.hasNext()) {
-            User user = users.next();
-
-            if (user.getName() == name) {
-                return Optional.empty();
-            }
+        if (byName.containsKey(name)) {
+            return Optional.empty();
         }
 
         User user = new User(new UserId(), name, sessionId);
         byId.put(user.getId(), user);
+        byName.put(user.getName(), user);
+        bySessionId.put(sessionId, user);
         return Optional.of(user);
-    }
-
-    public Optional<User> findBySessionId(SessionId sessionId) {
-        Iterator<User> users = byId.elements().asIterator();
-        while(users.hasNext()) {
-            User user = users.next();
-            Optional<SessionId> currentSessionId = user.getSessionId();
-            
-            if (currentSessionId.isEmpty()) {
-                continue;
-            }
-
-            if (currentSessionId.get() == sessionId) {
-                return Optional.of(user);
-            }
-        }
-        return Optional.empty();
-    }
-
-    // TODO: Add to EventRegistry with DisconnectEvent
-    public synchronized void onDisconnect(SessionId sessionId) {
-        Optional<User> user = findBySessionId(sessionId);
-
-        if (user.isPresent()) {
-            user.get().markDisconnected();
-        }
     }
 
     public synchronized boolean removeIfStillDisconnected(UserId userId) {
@@ -62,15 +33,33 @@ public class UserRegistry {
         }
 
         byId.remove(user.getId());
+        byName.remove(user.getName());
         return true;
+    }
+
+    // TODO: Add to EventRegistry with DisconnectEvent
+    public synchronized void onDisconnect(SessionId sessionId) {
+        User user = bySessionId.remove(sessionId);
+        if (user == null) {
+            return;
+        }
+
+        user.markDisconnected();
     }
 
     public synchronized Optional<User> reassignSession(UserId userId, SessionId sessionId) {
         User user = byId.get(userId);
-        if (user == null) return Optional.empty();
- 
+        if (user == null) {
+            return Optional.empty();
+        }
+
         user.reassignSession(sessionId);
+        bySessionId.put(sessionId, user);
         return Optional.of(user);
+    }
+
+    public Optional<User> findBySessionId(SessionId sessionId) {
+        return Optional.ofNullable(bySessionId.get(sessionId));
     }
 
     public Collection<User> getAllUsers() {
