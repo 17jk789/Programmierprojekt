edc1b29884bca41bdf2250c3d28914180481dcaf
Lars Simon Winzer <lars.winzer@stud.unibas.ch>
2026-03-24 18:11:37 +0100

    Add: PrimitiveRequest takes now RequestContext instead of requestId

--- src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/network/parser/PrimitiveRequest.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/network/parser/PrimitiveRequest.java
@@ -3,4 +3,5 @@ package ch.unibas.dmi.dbis.cs108.casono.server.network.parser;
 import java.util.List;
 
 /** Created by the ProtocolParser to allow easy access to the request contents */
-public record PrimitiveRequest(int requestId, String command, List<Parameter> parameters) {}
+public record PrimitiveRequest(
+        RequestContext context, String command, List<Parameter> parameters) {}
