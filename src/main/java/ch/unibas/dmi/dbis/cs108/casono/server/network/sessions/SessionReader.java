34ad8030733a61737b3b21d4eaafb7a677d2aa8a
Lars Simon Winzer <lars.winzer@stud.unibas.ch>
2026-03-24 18:07:20 +0100

    Refactor: ProtocolParser to accept payload string and return RawRequest
    Prior it took the RawPacket as an argument and returned an PrimitiveRequest

--- src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/network/sessions/SessionReader.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/network/sessions/SessionReader.java
@@ -2,9 +2,9 @@ package ch.unibas.dmi.dbis.cs108.casono.server.network.sessions;
 
 import ch.unibas.dmi.dbis.cs108.casono.server.network.events.DisconnectEvent;
 import ch.unibas.dmi.dbis.cs108.casono.server.network.events.EventBus;
-import ch.unibas.dmi.dbis.cs108.casono.server.network.parser.PrimitiveRequest;
 import ch.unibas.dmi.dbis.cs108.casono.server.network.parser.ProtocolParser;
 import ch.unibas.dmi.dbis.cs108.casono.server.network.parser.ProtocolParserException;
+import ch.unibas.dmi.dbis.cs108.casono.server.network.parser.RawRequest;
 import ch.unibas.dmi.dbis.cs108.casono.server.network.transport.RawPacket;
 import ch.unibas.dmi.dbis.cs108.casono.server.network.transport.TransportLayer;
 import ch.unibas.dmi.dbis.cs108.casono.server.tokenizer.TokenizerException;
@@ -36,8 +36,8 @@ public class SessionReader implements Runnable {
                 rawPacket = transport.read();
                 logger.debug("Recieved: {}", rawPacket);
 
-                PrimitiveRequest primitiveRequest = ProtocolParser.parse(rawPacket);
-                logger.debug("Parsed request to {}", primitiveRequest);
+                RawRequest rawRequest = ProtocolParser.parse(rawPacket.payload());
+                logger.debug("Parsed request to {}", rawRequest);
             } catch (EOFException e) {
                 logger.info("Client disconnected");
                 eventBus.publish(new DisconnectEvent(session.getId()));
