20047405416fa34ad97995399adbf09a71d8efa1
Lars Simon Winzer <lars.winzer@stud.unibas.ch>
2026-03-26 17:53:19 +0100

    Add: CommandHandler interface to be generic

--- src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/network/command/CommandHandler.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/network/command/CommandHandler.java
@@ -2,6 +2,6 @@ package ch.unibas.dmi.dbis.cs108.casono.server.network.command;
 
 import ch.unibas.dmi.dbis.cs108.casono.server.network.parser.Request;
 
-public interface CommandHandler {
-    void execute(Request request);
+public interface CommandHandler<T extends Request> {
+    void execute(T request);
 }
