9a6ad9f1bb2b857c083c0cc015494e2daaf41d28
Lars Simon Winzer <lars.winzer@stud.unibas.ch>
2026-03-26 17:59:41 +0100

    Add: Create CommandRouter at server startup and pass to SessionManager

--- src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/ServerApp.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/ServerApp.java
@@ -3,6 +3,8 @@ package ch.unibas.dmi.dbis.cs108.casono.server;
 import ch.unibas.dmi.dbis.cs108.casono.server.domain.user.UserCleanupJob;
 import ch.unibas.dmi.dbis.cs108.casono.server.domain.user.UserRegistry;
 import ch.unibas.dmi.dbis.cs108.casono.server.network.NetworkManager;
+import ch.unibas.dmi.dbis.cs108.casono.server.network.command.CommandRouter;
+import ch.unibas.dmi.dbis.cs108.casono.server.network.command.TestCommandHandler;
 import ch.unibas.dmi.dbis.cs108.casono.server.network.events.DisconnectEvent;
 import ch.unibas.dmi.dbis.cs108.casono.server.network.events.EventBus;
 import ch.unibas.dmi.dbis.cs108.casono.server.network.parser.CommandParserDispatcher;
@@ -28,7 +30,9 @@ public class ServerApp {
 
         EventBus eventBus = new EventBus();
         CommandParserDispatcher dispatcher = new CommandParserDispatcher();
-        SessionManager sessionManager = new SessionManager(eventBus, dispatcher);
+        CommandRouter router = new CommandRouter();
+
+        SessionManager sessionManager = new SessionManager(eventBus, dispatcher, router);
         eventBus.subscribe(DisconnectEvent.class, event -> sessionManager.onDisconnect(event));
         NetworkManager networkManager = new NetworkManager(port, sessionManager);
 
