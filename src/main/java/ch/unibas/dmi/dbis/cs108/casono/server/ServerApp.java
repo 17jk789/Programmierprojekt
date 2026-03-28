fc4a88ee95afc1443cf3ab2a671ce0bead497987
Lars Simon Winzer <lars.winzer@stud.unibas.ch>
2026-03-28 13:12:30 +0100

    Add: SessionDisconnectJob scheduling to ServerApp

--- src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/ServerApp.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/ServerApp.java
@@ -7,6 +7,7 @@ import ch.unibas.dmi.dbis.cs108.casono.server.network.command.CommandRouter;
 import ch.unibas.dmi.dbis.cs108.casono.server.network.events.DisconnectEvent;
 import ch.unibas.dmi.dbis.cs108.casono.server.network.events.EventBus;
 import ch.unibas.dmi.dbis.cs108.casono.server.network.parser.CommandParserDispatcher;
+import ch.unibas.dmi.dbis.cs108.casono.server.network.sessions.SessionDisconnectJob;
 import ch.unibas.dmi.dbis.cs108.casono.server.network.sessions.SessionManager;
 import java.time.Duration;
 import java.util.concurrent.Executors;
@@ -17,9 +18,12 @@ import org.apache.logging.log4j.Logger;
 
 /** Application class for starting the server. */
 public class ServerApp {
-    public static final int USER_CLEANUP_JOB_DELAY = 0;
-    public static final int USER_CLEANUP_JOB_PERIOD = 10;
-    public static final int USER_CLEANUP_JOB_RECONNECT_THRESHOLD = 10;
+    private static final int USER_CLEANUP_JOB_DELAY = 0;
+    private static final int USER_CLEANUP_JOB_PERIOD = 10;
+    private static final int USER_CLEANUP_JOB_RECONNECT_THRESHOLD = 10;
+    private static final int SESSION_DISCONNECT_JOB_DELAY = 0;
+    private static final int SESSION_DISCONNECT_JOB_PERIOD = 2;
+    private static final int SESSION_DISCONNECT_JOB_TIMEOUT = 5;
 
     public static void start(String arg) {
         int port = Integer.parseInt(arg);
@@ -44,6 +48,14 @@ public class ServerApp {
                         userRegistry, Duration.ofSeconds(USER_CLEANUP_JOB_RECONNECT_THRESHOLD)),
                 USER_CLEANUP_JOB_DELAY,
                 USER_CLEANUP_JOB_PERIOD,
+                TimeUnit.SECONDS);
+        scheduler.scheduleAtFixedRate(
+                new SessionDisconnectJob(
+                        sessionManager,
+                        eventBus,
+                        Duration.ofSeconds(SESSION_DISCONNECT_JOB_TIMEOUT)),
+                SESSION_DISCONNECT_JOB_DELAY,
+                SESSION_DISCONNECT_JOB_PERIOD,
                 TimeUnit.SECONDS);
 
         networkManager.start();
