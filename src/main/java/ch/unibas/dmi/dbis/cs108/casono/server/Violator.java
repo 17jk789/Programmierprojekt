9d4afdc3e8a7a6e63016dff0d3be1d6dc26f18c6
Lars Simon Winzer <lars.winzer@stud.unibas.ch>
2026-03-30 14:20:01 +0200

    Remove: Violator dummy file

--- src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/Violator.java
+++
@@ -1,7 +0,0 @@
-package ch.unibas.dmi.dbis.cs108.casono.server;
-
-public class Violator {
-    public void dummy() {
-        System.out.println("This is a disallowed sysout statement, triggering the failure of the merge request");
-    }
-}
