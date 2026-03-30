ed914941297f8b0a6c692bddadb23cc51f4705da
Lars Simon Winzer <lars.winzer@stud.unibas.ch>
2026-03-30 12:38:44 +0200

    Add: Exception to throw when error during parsing occurs

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/network/parser/ParameterParseException.java
@@ -0,0 +1,7 @@
+package ch.unibas.dmi.dbis.cs108.casono.server.network.parser;
+
+public class ParameterParseException extends RuntimeException {
+    public ParameterParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
