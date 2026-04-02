42b53440593ead1e74000170631d0a4dcb35e768
Jona Walpert <jona.walpert@stud.unibas.ch>
2026-04-02 22:28:12 +0200

    Test: Add in-process TestServer and update LobbyButtonGridManagerTest
    Add TestServer that speaks the RawPacket/TcpTransport protocol for CREATE_LOBBY,
    GET_LOBBY_STATUS and JOIN_LOBBY. Update LobbyButtonGridManagerTest to start the
    TestServer and exercise a real `ClientService(host, port)`, enabling integration-style
    tests of the client->transport->server roundtrip without an external server.
    
    Refs #36

--- src/test/java/ch/unibas/dmi/dbis/cs108/casono/client/ui/lobbyui/LobbyButtonGridManagerTest.java
+++ src/test/java/ch/unibas/dmi/dbis/cs108/casono/client/ui/lobbyui/LobbyButtonGridManagerTest.java
@@ -3,19 +3,32 @@ package ch.unibas.dmi.dbis.cs108.casono.client.ui.lobbyui;
 import static org.junit.jupiter.api.Assertions.*;
 
 import javafx.scene.layout.GridPane;
+import ch.unibas.dmi.dbis.cs108.casono.client.network.ClientService;
 import org.junit.jupiter.api.*;
 
 class LobbyButtonGridManagerTest {
     LobbyButtonGridManager gridManager;
     LobbyButtonTranslationManager translationManager;
     GridPane gridPane;
+    ch.unibas.dmi.dbis.cs108.casono.client.network.TestServer testServer;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception {
         gridPane = new GridPane();
         translationManager = LobbyButtonTranslationManager.getInstance();
         translationManager.getButtonIdToLobbyId().clear();
-        gridManager = new LobbyButtonGridManager(gridPane, translationManager);
+        // Start an in-process test server and connect a real ClientService to it.
+        testServer = new ch.unibas.dmi.dbis.cs108.casono.client.network.TestServer();
+        String host = "127.0.0.1";
+        int port = testServer.getPort();
+        ClientService clientService = new ClientService(host, port);
+        gridManager = new LobbyButtonGridManager(gridPane, translationManager, clientService);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        if (testServer != null)
+            testServer.close();
     }
 
     @Test
