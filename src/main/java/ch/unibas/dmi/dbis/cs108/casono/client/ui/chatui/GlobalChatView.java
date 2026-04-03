39f60c91134e9040f177c46a3cb020c7bcfb8057
Mathis Ginkel <m.ginkel@stud.unibas.ch>
2026-04-03 15:52:03 +0200

    Fix: small changes to ChatUI

--- src/main/java/ch/unibas/dmi/dbis/cs108/casono/client/ui/chatui/GlobalChatView.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/client/ui/chatui/GlobalChatView.java
@@ -4,6 +4,8 @@ import ch.unibas.dmi.dbis.cs108.casono.client.chat.ChatController;
 import ch.unibas.dmi.dbis.cs108.casono.client.chat.ChatModel;
 import ch.unibas.dmi.dbis.cs108.casono.client.chat.ChatType;
 import ch.unibas.dmi.dbis.cs108.casono.client.chat.Message;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -28,6 +30,8 @@ public class GlobalChatView {
 
     @FXML private ScrollPane globalChatScrollPane;
 
+    final BooleanProperty isContentVisible = new SimpleBooleanProperty(true);
+
     private static final int CHAT_PADDING = 20;
 
     public GlobalChatView(String username, ChatModel globalChatModel, ChatController controller) {
@@ -36,11 +40,11 @@ public class GlobalChatView {
         this.globalChatModel = globalChatModel;
     }
 
-    @FXML
     public void initialize() {
         globalChatInputField.setOnAction(event -> sendGlobalMessage());
         globalChatSendButton.setOnAction(event -> sendGlobalMessage());
         globalChatScrollPane.vvalueProperty().bind(globalChatVBox.heightProperty());
+        globalChatVBox.visibleProperty().bind(isContentVisible);
     }
 
     public void sendGlobalMessage() {
@@ -48,7 +52,7 @@ public class GlobalChatView {
         if (!message.isEmpty()) {
             globalChatInputField.clear();
             Message msg = new Message(ChatType.GLOBAL, 0, username, null, message);
-            controller.sendMessageToNetwork(msg);
+            controller.onSendToNetwork(msg);
         }
     }
 
@@ -64,6 +68,6 @@ public class GlobalChatView {
     }
 
     public void setVisibility(Boolean bool) {
-        globalChatVBox.setVisible(bool);
+        this.isContentVisible.set(bool);
     }
 }
