160ad8f2bbd4ebc8390a55395171803e7882e323
Lars Simon Winzer <lars.winzer@stud.unibas.ch>
2026-04-07 20:12:22 +0200

    Docs: Write JavaDoc for components related to LOGIN command

--- src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/app/commands/login/LoginHandler.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/app/commands/login/LoginHandler.java
@@ -2,23 +2,43 @@ package ch.unibas.dmi.dbis.cs108.casono.server.app.commands.login;
 
 import ch.unibas.dmi.dbis.cs108.casono.server.domain.user.User;
 import ch.unibas.dmi.dbis.cs108.casono.server.domain.user.UserFactory;
+import ch.unibas.dmi.dbis.cs108.casono.server.domain.user.UserId;
 import ch.unibas.dmi.dbis.cs108.casono.server.domain.user.UserRegistry;
 import ch.unibas.dmi.dbis.cs108.casono.server.network.command.execution.CommandHandler;
 import ch.unibas.dmi.dbis.cs108.casono.server.network.protocol.response.ErrorResponse;
 import ch.unibas.dmi.dbis.cs108.casono.server.network.protocol.response.dispatcher.ResponseDispatcher;
 import java.util.Optional;
 
+/**
+ * Handles {@link LoginRequest}s to create a user for a session, if the session has not assigned one
+ * already
+ */
 public class LoginHandler implements CommandHandler<LoginRequest> {
     private final ResponseDispatcher responseDispatcher;
     private final UserRegistry userRegistry;
     private final UserFactory userFactory;
 
+    /**
+     * Creates a new handler for checking for existing user and creating a new one
+     *
+     * @param responseDispatcher the dispatcher used to send the response
+     * @param userRegistry the registry used to look up existing users and create the new one
+     */
     public LoginHandler(ResponseDispatcher responseDispatcher, UserRegistry userRegistry) {
         this.responseDispatcher = responseDispatcher;
         this.userRegistry = userRegistry;
         this.userFactory = new UserFactory(userRegistry);
     }
 
+    /**
+     * Executes the login request
+     *
+     * <p>If no user is already assigned to the session, a new user is created and its name and
+     * {@link UserId} returned in the response. If the session has already a user assigned, a {@code
+     * ALREADY_LOGGED_IN} error is responded with.
+     *
+     * @param request the request to execute
+     */
     @Override
     public void execute(LoginRequest request) {
         Optional<User> existingUser = userRegistry.getBySessionId(request.getSessionId());
--- src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/app/commands/login/LoginParser.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/app/commands/login/LoginParser.java
@@ -4,7 +4,14 @@ import ch.unibas.dmi.dbis.cs108.casono.server.network.command.parsing.CommandPar
 import ch.unibas.dmi.dbis.cs108.casono.server.network.protocol.request.PrimitiveRequest;
 import ch.unibas.dmi.dbis.cs108.casono.server.network.protocol.request.accessor.RequestParameterAccessor;
 
+/** Parses a primitive request into a {@link LoginRequest}. */
 public class LoginParser implements CommandParser<LoginRequest> {
+    /**
+     * Extracts the required {@code USERNAME} parameter from the incoming request.
+     *
+     * @param primitiveRequest the request to parse
+     * @return {@link LoginRequest} containing the username
+     */
     @Override
     public LoginRequest parse(PrimitiveRequest primitiveRequest) {
         RequestParameterAccessor accessor =
