ea41a3c75a770038fb21ebed00a0facb2f3b0884
Julian Kropff <j.kropff@stud.unibas.ch>
2026-04-10 20:18:20 +0200

    Add: PlayerState class Refs #52

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/client/game/PlayerState.java
@@ -0,0 +1,10 @@
+package ch.unibas.dmi.dbis.cs108.casono.client.game;
+
+/**
+ * Enumeration representing the possible states of a player in the poker game.
+ */
+public enum PlayerState {
+    ACTIVE,
+    FOLDED,
+    DEALER
+}
