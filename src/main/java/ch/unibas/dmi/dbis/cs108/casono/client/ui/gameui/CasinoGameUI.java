a2d176f13c2cfa254f03fe2ff72bf8d141bd0e51
Julian Kropff <j.kropff@stud.unibas.ch>
2026-04-10 20:34:49 +0200

    Refactor: improve CasinoGameUI structure Refs #52

--- src/main/java/ch/unibas/dmi/dbis/cs108/casono/client/ui/gameui/CasinoGameUI.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/client/ui/gameui/CasinoGameUI.java
@@ -1,5 +1,6 @@
 package ch.unibas.dmi.dbis.cs108.casono.client.ui.gameui;
 
+import ch.unibas.dmi.dbis.cs108.casono.client.network.ClientService;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -9,21 +10,27 @@ import javafx.stage.Stage;
 /**
  * Main class for the Casono Game UI.
  *
- * <p>Starts the JavaFX application, loads the graphical user interface from the FXML file, and
+ * Starts the JavaFX application, loads the graphical user interface from the FXML file, and
  * initializes the main stage for the game.
  *
- * <p>Tasks: - Loads the FXML interface "/ui-structure/Casinogameui.fxml". - Loads the application
+ * Tasks: - Loads the FXML interface "/ui-structure/Casinogameui.fxml". - Loads the application
  * icon from "/images/logoinverted.png". - Starts the application in full-screen mode.
  */
 public class CasinoGameUI extends Application {
+
+    private static ClientService clientService;
+
+    private static final int DEFAULT_WIDTH = 1200;
+    private static final int DEFAULT_HEIGHT = 800;
 
     /** default constructor */
     public CasinoGameUI() {
         // default no-arg constructor
     }
 
-    private static final int DEFAULT_WIDTH = 1200;
-    private static final int DEFAULT_HEIGHT = 800;
+    public static void setClientService(ClientService clientService) {
+        CasinoGameUI.clientService = clientService;
+    }
 
     /**
      * Starts the main stage of the application.
@@ -36,7 +43,7 @@ public class CasinoGameUI extends Application {
         FXMLLoader fxmlLoader =
                 new FXMLLoader(CasinoGameUI.class.getResource("/ui-structure/Casinogameui.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), DEFAULT_WIDTH, DEFAULT_HEIGHT);
-        stage.setTitle("Casono (GAME)");
+        stage.setTitle("Casono");
 
         String iconPath = getClass().getResource("/images/logoinverted.png").toExternalForm();
         stage.getIcons().add(new javafx.scene.image.Image(iconPath));
