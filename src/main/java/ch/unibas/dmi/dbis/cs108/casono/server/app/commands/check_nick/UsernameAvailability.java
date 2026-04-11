69333ede7ab51ad92b76fa7d1e6622b4566b2b71
Mathis Ginkel <m.ginkel@stud.unibas.ch>
2026-04-11 14:48:46 +0200

    Fix: change visibility of enum UsernameAvailability Refs #88

--- src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/app/commands/check_nick/UsernameAvailability.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/server/app/commands/check_nick/UsernameAvailability.java
@@ -1,10 +1,8 @@
 package ch.unibas.dmi.dbis.cs108.casono.server.app.commands.check_nick;
-
 /** Represents the availability status of a username */
-public enum UsernameAvailability {
+enum UsernameAvailability {
     /** Username is available */
     FREE,
-
     /** Username is already in use */
     TAKEN
 }
