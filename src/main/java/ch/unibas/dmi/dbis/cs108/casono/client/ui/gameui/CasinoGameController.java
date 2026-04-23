0f4d048b767023d0bb5e1b713433f4c39c474105
Julian Kropff <j.kropff@stud.unibas.ch>
2026-04-23 19:59:55 +0200

    Fix: Chat UI initialization improved in Game UI

--- src/main/java/ch/unibas/dmi/dbis/cs108/casono/client/ui/gameui/CasinoGameController.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/client/ui/gameui/CasinoGameController.java
@@ -143,7 +143,6 @@ public class CasinoGameController {
     private static final double CHAT_WIDTH = 400;
     private static final double CHAT_HEIGHT = 600;
 
-    private ChatController chatController;
     private String chatUsername;
     private ClientService chatClientService;
     private int chatLobbyId = -1;
