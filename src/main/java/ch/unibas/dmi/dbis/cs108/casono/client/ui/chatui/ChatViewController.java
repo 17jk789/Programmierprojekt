d8d6dbb34f0750c616e9f90fe1058de30e476e3e
Mathis Ginkel <m.ginkel@stud.unibas.ch>
2026-04-26 22:19:57 +0200

    Style: fixing some linter warnings in chat ui classes

--- src/main/java/ch/unibas/dmi/dbis/cs108/casono/client/ui/chatui/ChatViewController.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/casono/client/ui/chatui/ChatViewController.java
@@ -13,7 +13,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 /** Responsible for the presentation of the ChatModel to the Client */
@@ -23,10 +22,6 @@ public class ChatViewController implements Initializable {
     @FXML private Button sendButton;
 
     @FXML private TextField inputField;
-
-    @FXML private VBox chatInterfaceVBox;
-
-    @FXML private HBox controlBar;
 
     @FXML private ScrollPane scrollPane;
 
@@ -46,8 +41,6 @@ public class ChatViewController implements Initializable {
 
     private final ChatModel chatModel;
 
-    private final String username;
-
     private final ChatController controller;
 
     private static final int CHAT_PADDING = 20;
@@ -55,10 +48,8 @@ public class ChatViewController implements Initializable {
     public ChatViewController(
             ChatController chatController,
             ChatModel chatModel,
-            String username,
             ChatBoxController chatBoxController) {
         this.controller = chatController;
-        this.username = username;
         this.chatModel = chatModel;
         this.chatBoxController = chatBoxController;
         tabIsOpen = true;
@@ -74,8 +65,8 @@ public class ChatViewController implements Initializable {
      */
     @Override
     public void initialize(URL location, ResourceBundle resourceBundle) {
-        inputField.setOnAction(event -> sendMessage());
-        sendButton.setOnAction(event -> sendMessage());
+        inputField.setOnAction(_ -> sendMessage());
+        sendButton.setOnAction(_ -> sendMessage());
         scrollPane.vvalueProperty().bind(chat.heightProperty());
     }
 
